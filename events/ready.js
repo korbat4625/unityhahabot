ee8663f5f57306f3959ab5ec1806a0960f1e42ad
= <=>
2021-12-31 05:40:23 +0800

    test

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client, callback) {
-		console.log(`成功登入! Logged in as ${client.user.tag}\n`);
+		console.log(`成功登入... Logged in as ${client.user.tag}\n`);
 		const clientId = client.application.id
 		const guildsId = [];
 		for(var attr of client.guilds.cache) {
